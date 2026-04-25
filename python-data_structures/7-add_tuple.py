0c9d8d7aaadf3e5d7b7447be20e0b6b5178c00b4
Banuchichak <mbbm13245@gmail.com>
2026-04-25 08:59:31 +0000

    Fix pycodestyle E501 and W293 in Task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Tuple-ları ən azı 2 elementli hala gətiririk (çatışmayan yerə 0 əlavə edirik)
-    # Əgər element 2-dən çoxdursa, [:2] ilə ilk ikisini götürürük
+    # Ensure tuples have at least 2 elements by padding with 0s
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    
+
     res_1 = a[0] + b[0]
     res_2 = a[1] + b[1]
-    
+
     return (res_1, res_2)
