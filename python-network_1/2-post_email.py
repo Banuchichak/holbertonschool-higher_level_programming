cec3b9a9508e92bc9a03d3bfd4cfb9ce17e38a8b
Banuchichak <mbbm13245@gmail.com>
2026-04-25 11:40:17 +0000

    Add Task 2: POST an email using urllib

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to a URL with an email as a parameter
+and displays the body of the response (decoded in utf-8).
+"""
+import urllib.parse
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+
+    # Məlumatın URL-encoded formatına salınması
+    data = urllib.parse.urlencode(values)
+    # Məlumatın baytlara çevrilməsi (POST sorğusu üçün vacibdir)
+    data = data.encode('ascii')
+    
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
